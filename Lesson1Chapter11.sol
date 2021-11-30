0af8e8f653dfd948f84dd826deb8e1a402eee531
Yuvraj Chandra <Singhyuvraj179@gmail.com>
2021-12-01 00:17:44 +0530

    Create Lesson1Chapter11.sol

---
+++ Lesson1Chapter11.sol
@@ -0,0 +1,25 @@
+// Chapter 11: Keccak256 and Typecasting
+pragma solidity ^0.4.25;
+
+contract ZombieFactory {
+
+    uint dnaDigits = 16;
+    uint dnaModulus = 10 ** dnaDigits;
+
+    struct Zombie {
+        string name;
+        uint dna;
+    }
+
+    Zombie[] public zombies;
+
+    function _createZombie(string memory _name, uint _dna) private {
+        zombies.push(Zombie(_name, _dna));
+    }
+
+    function _generateRandomDna(string memory _str) private view returns (uint) {
+        uint rand = uint(keccak256(abi.encodePacked(_str)));
+        return rand % dnaModulus;
+    }
+
+}
