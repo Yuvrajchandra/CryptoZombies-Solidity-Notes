0708b1a7fef3f2563a3934d9abe6952e8aa455c0
Yuvraj Chandra <Singhyuvraj179@gmail.com>
2021-12-01 00:00:55 +0530

    Create Lesson1Chapter9.sol

---
+++ Lesson1Chapter9.sol
@@ -0,0 +1,20 @@
+// Chapter 9: Private / Public Functions
+pragma solidity >=0.5.0 <0.6.0;
+
+contract ZombieFactory {
+
+    uint dnaDigits = 16;
+    uint dnaModulus = 10 ** dnaDigits;
+
+    struct Zombie {
+        string name;
+        uint dna;
+    }
+
+    Zombie[] public zombies;
+
+    function _createZombie(string memory _name, uint _dna) private {
+        zombies.push(Zombie(_name, _dna));
+    }
+
+}
