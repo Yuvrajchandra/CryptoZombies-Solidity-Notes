a0e66762ef84b6a75f1f068a4a53f60422825556
Yuvraj Chandra <Singhyuvraj179@gmail.com>
2021-11-30 23:32:32 +0530

    Create Lesson1Chapter6.sol

---
+++ Lesson1Chapter6.sol
@@ -0,0 +1,16 @@
+// Chapter 6: Arrays
+pragma solidity >=0.5.0 <0.6.0;
+
+contract ZombieFactory {
+
+    uint dnaDigits = 16;
+    uint dnaModulus = 10 ** dnaDigits;
+
+    struct Zombie {
+        string name;
+        uint dna;
+    }
+
+    Zombie[] public zombies;
+
+}
