50324b254602a9edef0daea229b478a433c32454
Yuvraj Chandra <Singhyuvraj179@gmail.com>
2021-12-03 00:07:01 +0530

    Create Lesson2Chapter7.sol

---
+++ Lesson2Chapter7.sol
@@ -0,0 +1,13 @@
+// Storage vs Memory (Data location)
+pragma solidity >=0.5.0 <0.6.0;
+
+import "./zombiefactory.sol";
+
+contract ZombieFeeding is ZombieFactory {
+
+  function feedAndMultiply(uint _zombieId, uint _targetDna) public {
+    require(msg.sender == zombieToOwner[_zombieId]);
+    Zombie storage myZombie = zombies[_zombieId];
+  }
+
+}
