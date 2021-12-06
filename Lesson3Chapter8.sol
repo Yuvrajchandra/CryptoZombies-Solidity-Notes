90d4703d2873b6462ecc3631575a3b9b502a87d1
Yuvraj Chandra <Singhyuvraj179@gmail.com>
2021-12-06 20:12:44 +0530

    Create Lesson3Chapter8.sol

---
+++ Lesson3Chapter8.sol
@@ -0,0 +1,12 @@
+pragma solidity >=0.5.0 <0.6.0;
+
+import "./zombiefeeding.sol";
+
+contract ZombieHelper is ZombieFeeding {
+
+  modifier aboveLevel(uint _level, uint _zombieId) {
+    require(zombies[_zombieId].level >= _level);
+    _;
+  }
+
+}
