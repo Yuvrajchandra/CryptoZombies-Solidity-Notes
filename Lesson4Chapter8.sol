f4f1e6c3f423f58e0aca860d80c2374fa65e4eb6
Yuvraj Chandra <Singhyuvraj179@gmail.com>
2021-12-18 23:49:32 +0530

    Create Lesson4Chapter8.sol

---
+++ Lesson4Chapter8.sol
@@ -0,0 +1,21 @@
+pragma solidity >=0.5.0 <0.6.0;
+
+import "./zombiehelper.sol";
+
+contract ZombieAttack is ZombieHelper {
+  uint randNonce = 0;
+  uint attackVictoryProbability = 70;
+
+  function randMod(uint _modulus) internal returns(uint) {
+    randNonce++;
+    return uint(keccak256(abi.encodePacked(now, msg.sender, randNonce))) % _modulus;
+
+  }
+
+  // 1. Add modifier here
+  function attack(uint _zombieId, uint _targetId) external ownerOf(_zombieId) {
+    Zombie storage myZombie = zombies[_zombieId];
+    Zombie storage enemyZombie = zombies[_targetId];
+    uint rand = randMod(100);
+  }
+}
