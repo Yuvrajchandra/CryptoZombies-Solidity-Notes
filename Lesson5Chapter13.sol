98aad410b2bb2bc6df916438b207ede55d6f4684
Yuvraj Chandra <Singhyuvraj179@gmail.com>
2021-12-19 22:13:29 +0530

    Create Lesson5Chapter13.sol

---
+++ Lesson5Chapter13.sol
@@ -0,0 +1,42 @@
+pragma solidity >=0.5.0 <0.6.0;
+
+import "./zombieattack.sol";
+import "./erc721.sol";
+import "./safemath.sol";
+
+/// TODO: Replace this with natspec descriptions
+/// @title A contract that manages transfering zombie ownership
+/// @author Yuvraj Chandra
+/// @dev Compliant with OpenZeppelin's implementation of the ERC721 spec draft
+contract ZombieOwnership is ZombieAttack, ERC721 {
+
+  using SafeMath for uint256;
+
+  mapping (uint => address) zombieApprovals;
+
+  function balanceOf(address _owner) external view returns (uint256) {
+    return ownerZombieCount[_owner];
+  }
+
+  function ownerOf(uint256 _tokenId) external view returns (address) {
+    return zombieToOwner[_tokenId];
+  }
+
+  function _transfer(address _from, address _to, uint256 _tokenId) private {
+    ownerZombieCount[_to] = ownerZombieCount[_to].add(1);
+    ownerZombieCount[msg.sender] = ownerZombieCount[msg.sender].sub(1);
+    zombieToOwner[_tokenId] = _to;
+    emit Transfer(_from, _to, _tokenId);
+  }
+
+  function transferFrom(address _from, address _to, uint256 _tokenId) external payable {
+    require (zombieToOwner[_tokenId] == msg.sender || zombieApprovals[_tokenId] == msg.sender);
+    _transfer(_from, _to, _tokenId);
+  }
+
+  function approve(address _approved, uint256 _tokenId) external payable onlyOwnerOf(_tokenId) {
+    zombieApprovals[_tokenId] = _approved;
+    emit Approval(msg.sender, _approved, _tokenId);
+  }
+
+}
